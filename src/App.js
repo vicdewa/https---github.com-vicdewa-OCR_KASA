cc7888d5a95f84a664ae831ea3f87f819f1cf988
Victoire Dewaegeneire <victoire.dew@gmail.com>
2024-09-26 23:38:52 +0200

    Etape 4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './components/Home';   
+import Logement from './components/Logement';
+import About from './components/About';
+import Error from './components/Error';  
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/logement" element={<Logement />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Error />} />  
+      </Routes>
+    </Layout>
+
   );
 }
 
